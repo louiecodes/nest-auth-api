93b5e4b1eff8a476afec803855e93bd6d3435c51
L O U I S <luis_henano@hotmail.com>
2024-06-13 00:28:18 -0300

    deleted AccessTokenGuard from UserController as its not needed anymore

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,7 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetCurrentUser } from 'src/commons/decorators';
-import { AccessTokenGuard } from 'src/commons/guards';
 
-@UseGuards(AccessTokenGuard)
 @Controller('users')
 export class UserController {
   @Get('me')
